6269050fe617656a8b16d3fbc7df26b471fad3f3
Lucas <hairmare@rabe.ch>
2023-10-10 21:00:27 +0200

    feat: update backstage to 1.18

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -42,7 +42,7 @@ function makeCreateEnv(config: Config) {
   const cacheManager = CacheManager.fromConfig(config);
   const databaseManager = DatabaseManager.fromConfig(config, { logger: root });
   const tokenManager = ServerTokenManager.fromConfig(config, { logger: root });
-  const taskScheduler = TaskScheduler.fromConfig(config);
+  const taskScheduler = TaskScheduler.fromConfig(config, { databaseManager });
 
   const identity = DefaultIdentityClient.create({
     discovery,
